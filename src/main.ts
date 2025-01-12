1528087aa4fa9412afffd94fca60126bd803094a
Alexander Cai <alexandercai@outlook.com>
2025-01-11 18:39:35 -0700

    basic physics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { createCube, createFloor } from './objects';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import * as RAPIER from '@dimforge/rapier3d';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -11,34 +12,49 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-// Add orbit controls
+// add orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // Add smooth damping effect
+controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.screenSpacePanning = false;
 controls.minDistance = 1;
 controls.maxDistance = 50;
-controls.maxPolarAngle = Math.PI / 2; // Prevent camera from going below the ground
+controls.maxPolarAngle = Math.PI / 2;
+
+// initialize physics
+const gravity = { x: 0.0, y: -9.81, z: 0.0 };
+const world = new RAPIER.World(gravity);
 
 
-addObjects();
+const dynamicObjects = addObjects();
 
 camera.position.set(0, 8, 2);
 camera.lookAt(0, 0, 0);
 
 function animate() {
   controls.update();
+  world.step();
   renderer.render(scene, camera);
+
+  dynamicObjects.forEach(({ piece, body }) => {
+    const p = body.translation();
+    piece.position.set(p.x, p.y, p.z);
+    const q = body.rotation();
+    piece.setRotationFromQuaternion(new THREE.Quaternion(q.x, q.y, q.z, q.w));
+  });
 }
 
 function addObjects() {
-  const floor = createFloor({});
-  scene.add(floor);
+  const objects = [];
+
+  createFloor(scene, world, {});
 
   for (let x = -3; x <= 3; x += 1) {
     for (let z = -3; z <= 3; z += 1) {
-      const cube = createCube({ x, y: 0.5, z });
-      scene.add(cube);
+      const body = createCube(scene, world, { x, y: 2, z });
+      objects.push(body);
     }
   }
+
+  return objects;
 }
--- src/objects.ts
+++ src/objects.ts
@@ -1,29 +1,34 @@
 import * as THREE from 'three';
+import * as RAPIER from '@dimforge/rapier3d'
 
 
-export function createFloor({
-    size = 200,
-    divisions = 100,
+export function createFloor(scene: THREE.Scene, world: RAPIER.World, {
+    size = 20,
+    divisions = 10,
     color1 = 0x0000ff,
     color2 = 0x808080,
 }) {
+    // render
     const gridHelper = new THREE.GridHelper(
         size,
         divisions,
         color1,
         color2
     )
+    scene.add(gridHelper);
 
-    return gridHelper;
+    // physics
+    const floorColliderDesc = RAPIER.ColliderDesc.cuboid(size, 1, size);
+    world.createCollider(floorColliderDesc);
 }
 
-export function createCube({
-    x = 0, y = 0, z = 0,
+export function createCube(scene: THREE.Scene, world: RAPIER.World, {
+    x = 0, y = 0, z = 0, size = 1
 }) {
     const group = new THREE.Group();
 
     // cube
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const geometry = new THREE.BoxGeometry(size, size, size);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
 
@@ -37,7 +42,24 @@ export function createCube({
     group.add(cube)
     group.add(outline)
 
-    group.position.set(x, y, z)
+    group.position.set(x, y, z);
+    group.rotateOnAxis(new THREE.Vector3().randomDirection(), Math.random() * Math.PI);
 
-    return group;
+    scene.add(group);
+
+    // physics
+    // rigid body
+    const bodyDesc = RAPIER.RigidBodyDesc.dynamic();
+    bodyDesc.setTranslation(x, y, z);
+    bodyDesc.setRotation(group.quaternion);
+    const body = world.createRigidBody(bodyDesc);
+
+    // collider
+    const colliderDesc = RAPIER.ColliderDesc.cuboid(size / 2, size / 2, size / 2);
+    world.createCollider(colliderDesc, body);
+
+    return {
+        piece: group,
+        body,
+    };
 }
